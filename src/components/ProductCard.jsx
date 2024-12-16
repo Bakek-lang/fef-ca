f353341dbf3d670d6f4105a992424e8ccb4abfa8
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-12-16 14:44:32 +0100

    product card and product page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function ProductCard({ product }) {
   return (
     <div className="rounded-lg shadow-lg max-w-sm flex gap-2">
@@ -9,7 +11,15 @@ export default function ProductCard({ product }) {
       <div>
         <h2 className="font-bold text-lg mb-2">{product.title}</h2>
         <p className="text-gray-700 mb-4">{product.description}</p>
-        <p className="text-gray-900 font-semibold">{product.price}</p>
+        <p className="text-gray-900 font-semibold mb-2">{product.price}</p>
+        <div className="items-center">
+          <Link
+            to={`/products/${product.id}`}
+            className=" bg-orange-400 p-2 rounded-lg "
+          >
+            View Product
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,3 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function ProductPage() {
-  return <div>Product PAGE</div>;
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function getData(url) {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setData(data.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData(`https://v2.api.noroff.dev/online-shop/${id}`);
+  }, [id]);
+
+  if (isLoading || !data) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    <div>Error</div>;
+  }
+
+  return (
+    <div className="flex flex-col items-center p-4">
+      <img
+        src={data.image.url}
+        alt={data.title}
+        className="max-w-md rounded-lg"
+      />
+      <div className="mt-4 text-center">
+        <h2>{data.title}</h2>
+        <p>{data.description}</p>
+        <p>{data.price}</p>
+      </div>
+      <button className="bg-orange-400 rounded-lg  font-bold py-2 px-4">
+        Add to Cart
+      </button>
+    </div>
+  );
 }
