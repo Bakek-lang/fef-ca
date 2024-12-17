3f984dfc2b1ba32dc7e337899b50926332a57fe0
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-12-17 06:12:45 +0100

    cart updates

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,8 +1,14 @@
 import { IoCartSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 export default function CartIcon({ itemCount }) {
-  console.log("itemcount in cartIcon component: ", itemCount);
+  const navigate = useNavigate();
+
+  function handleCartClick() {
+    navigate("/checkout");
+  }
+
   return (
-    <div className="relative">
+    <div className="relative cursor-pointer" onClick={handleCartClick}>
       <IoCartSharp size={42} />
       {itemCount > 0 && (
         <span className="absolute top-0 right-0 rounded-full bg-red-600 text-white h-4 w-4 flex justify-center items-center p-2">
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,3 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../components/CartProvider";
+
 export default function CheckoutPage() {
-  return <div>CHECKOUT PAGE</div>;
+  const { cartItems, getTotal } = useCart();
+  console.log("cartitems: ", cartItems, "GetTotal: ", getTotal);
+  const navigate = useNavigate();
+
+  function handleCheckout() {
+    navigate("/checkout-success");
+  }
+
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <ul>
+        {cartItems.map((item, index) => (
+          <li key={index}>
+            {item.title} - {item.price}
+          </li>
+        ))}
+      </ul>
+      <p>Total: {getTotal()}</p>
+      <button onClick={handleCheckout}>Checkout</button>
+    </div>
+  );
 }
