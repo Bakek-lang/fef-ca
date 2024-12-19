0e0776223874c459ade640ef8b9b3f61afbc743c
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-12-19 13:52:20 +0100

    typos

--- src/pages/CheckoutSuccessPage.jsx
+++ src/pages/CheckoutSuccessPage.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function CheckoutSuccessPage() {
   return (
     <div>
-      <h1>Checkout SuccessFul</h1>
+      <h1>Checkout Successful</h1>
       <p className="mb-4">
-        Your order has been placed. Than you for your purchase!
+        Your order has been placed. Thank you for your purchase!
       </p>
       <Link className="bg-orange-400 py-4 px-2 rounded-lg" to="/">
         Return to Store
